277f93559a1f565a92a7e57183a4821dbcf89875
Mateus Pereira <mateus.ps@yahoo.com>
2021-05-21 12:34:12 -0300

    Continuação do Curso

---
+++ Fundamentos/objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.nome = 'Mateus Pereira'
+prod1.valor = 10.0
+prod1['Desconto Valor'] = 0.40 // evitar atributos com espaços
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Patricia Pereira',
+    valor: 45.0
+}
+
+console.log(prod2)
---
+++ Fundamentos/usandoVar1.js
@@ -0,0 +1,20 @@
+{ { { { var sera= 'Será' } } } }
+
+console.log(sera)
+
+function test () {
+    var local = 123
+    console.log(local)
+}
+
+teste()
+console.log(local)
+
+/*
+ * Variáveis criadas com o Var fora de uma função
+ * são criadas de formas globais, podem ser usadas
+ * fora de um bloco.
+ * Se criada numa função, só será aceita dentro do
+ * bloco da função.
+
+*/
